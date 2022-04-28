b757e1e5b2d2864ab4bb7c045fe4160db1132b9d
Lior Shalev <liorshalev6@gmail.com>
2022-04-28 15:07:40 +0300

    add OMDb Api

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hi there!');
+//Using Axios
+
+const fetchData = async ()=>{
+    const response = await axios.get('http://www.omdbapi.com/',{
+        params:{
+            apikey: '6943a6e6',
+            i: "tt0848228"
+        }
+    });
+    console.log(response.data);
+}
+
+fetchData();
+
