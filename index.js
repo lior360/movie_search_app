1a040d82a7dcb810787f5eeb08aa4acfc3846272
Lior Shalev <liorshalev6@gmail.com>
2022-04-28 19:55:13 +0300

    print movies on screen

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //Using Axios
-
 const fetchData = async (searchTerm)=>{
     const response = await axios.get('http://www.omdbapi.com/',{
         params:{
@@ -7,26 +6,25 @@ const fetchData = async (searchTerm)=>{
             s: searchTerm
         }
     });
-    console.log(response.data);
+    return response.data.Search;
+
 }
 
-const debounce = (func,delay=1000)=>{
-    let timeoutId;
-    return (...args)=>{
-        if (timeoutId){
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(()=>{
-            func.apply(null,args);
-        },delay)
-    }
-}
 
 
 const input = document.querySelector('input');
 
-const onInput = debounce(event=>{
-        fetchData(event.target.value);
-});
+const onInput = async event=>{
+        const movies = await fetchData(event.target.value);
+        for (let movie of movies){
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <img src = "${movie.Poster}" />
+            <h1>${movie.Title}</h1>
+            `;
+
+            document.querySelector('#target').appendChild(div);
+        }
+};
 
 input.addEventListener('input',debounce(onInput,500));
